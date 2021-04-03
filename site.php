2e9fffc898d8ee7c5e3d4af748a7e57fc8699fd7
matthshenrique <matths1117@gmail.com>
2021-04-03 19:10:37 -0300

    Site - Produtos

--- site.php
+++ site.php
@@ -1,12 +1,17 @@
 <?php
 
+use \Hcode\Model\Product;
 use \Hcode\Page;
 
 //Rout Tela Principal do Site
 $app->get('/', function () {
 
+    $products = Product::listAll();
+
     $page = new Page();
 
-    $page->setTpl("index");
+    $page->setTpl("index", [
+        "products" => Product::checkList($products),
+    ]);
 
 });
